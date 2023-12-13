1a85a3efaa3ecc050f32dea61b3766f1f30faf6c
Clyde-Kareithi <klydedavid1@gmail.com>
2023-12-13 19:09:11 +0300

    Update

---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+export const LoginPage = () => {
+    return (
+        <div className="w-full bg-red">
+            <h2 className="text-4xl text-center">Hey, I am the login page.</h2>
+        </div>
+    )
+}
+
+export default LoginPage;
